9b42c5ac6a5e5e528ef2a2260a44cd651723eec7
Dezthro <mabo7991@gmail.com>
2017-06-11 14:00:19 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,58 @@
+package kVTree;
+
 import java.lang.Math;
+import java.util.Iterator;
+
+/**
+ * a class which creates a tree structure
+ * 
+ * @param <K> defines the type of the keys
+ * @param <V> defines the type of the values
+ * @author Marius Bonke 4839983 Gruppe 3b
+ * @author Lucas Schröder 4809832 Gruppe 3b
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        IntegerDataSource integers = new IntegerDataSource();
+        StringDatasource strings = new StringDataSource();
+        Iterator string = strings.iterator();
+        Iterator integer = integers.iterator();
+        
+        Mytree<String, Integer> tree = new MyTree();
+        System.out.println();
+        
+        // hinzufuegen und erfassen
+        while(string.hasNext() && integer.hasNext()) {
+        
+            tree.put(string.next(), integer.next());
+            System.out.println(tree.toString());
+            System.out.println();
+        
+        }
+        
+        // loeschen
+        Iterator treeIterator = tree.iterator();
+        MyTree tempTree = null;
+        
+        for(int i = 0; i < ((int) (Math.random() * 5)); i++) {
+        
+            if(treeIterator.hasNext()) {
+            
+                tempTree = treeIterator.next();
+            
+            }
+        
+        }
+        
+        System.out.println("geloeschter Wert: " + tempTree.remove());
+        System.out.println();
+        System.out.println(tree.toString());
+
+    }
+
+}import java.lang.Math;
 import java.util.Iterator;
 
 public class Main {
